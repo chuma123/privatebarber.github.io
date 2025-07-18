eaeabca7a52fac3abb368d08ac1318b9d0d478cd
Prince Elvis <princeelvisp95@gmail.com>
2025-07-18 08:46:11 +0200

    Add files via upload
    use this script instead

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==== CONFIG ====
 const users = {
   luvo: { password: "3430", haircuts: 4, used: 2, nextBooking: null },
-  peter: { password: "6882", haircuts: 4, used: 1, nextBooking: null },
+  peter: { password: "6882", haircuts: 4, used: 2, nextBooking: null },
   lesego: { password: "5820", haircuts: 4, used: 2, nextBooking: null },
   mac: { password: "4692", haircuts: 4, used: 1, nextBooking: null },
   themba: { password: "9333", haircuts: 5, used: 0, nextBooking: null },
