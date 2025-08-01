aa39460ecf903400cee64763f16bee03ebbd5fba
Prince Elvis <princeelvisp95@gmail.com>
2025-08-01 11:00:30 +0200

    Add files via upload
    use this script.js instead

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const users = {
   themba: { password: "9333", haircuts: 5, used: 3, nextBooking: null },
   daryl: { password: "6921", haircuts: 4, used: 2, nextBooking: null },
   mj: { password: "5434", haircuts: 8, used: 8, nextBooking: null },
-  kiid: {password: "1385", haircut: 4,used: 3, nextBooking: null },
+  kiid: {password: "1385", haircuts: 4,used: 3, nextBooking: null },
   
 };
 const adminPassword = "12314";
